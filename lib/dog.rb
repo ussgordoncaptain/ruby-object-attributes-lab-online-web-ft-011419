f4bd40332669de5c0dff3fb1b2270bc09bb5cb9c
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-26 09:38:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name 
+    @name
+  end
+  def name=(neo_name)
+    @name=neo_name
+  end
+  def breed 
+    @breed
+  end
+  def breed=(chosen_breed)
+    @breed= chosen_breed
+  end
+end
