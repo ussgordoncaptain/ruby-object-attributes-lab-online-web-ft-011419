ce13f2d7749891ffceeb3d9acef25f3d82c97db7
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-26 09:38:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,13 +2,13 @@ class Person
   def name 
     @name
   end
-  def name= (new_name)
-    @name= new_name
+  def name=(new_name)
+    @name = new_name
   end
   def job
     @job
   end
-  def job = (new_job)
+  def job=(new_job)
     @job=new_job
   end
 end
