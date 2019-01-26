ef2041bb503e4c39b118ce7a7799fe6f0c180d19
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-26 09:37:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ class Dog
   def name 
     @name
   end
-  def name = (neo_name)
+  def name=(neo_name)
     @name=neo_name
   end
   def breed 
     @breed
   end
-  def breed = (chosen_breed)
+  def breed=(chosen_breed)
     @breed= chosen_breed
   end
 end
